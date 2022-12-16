5dc35cadd4cfff06600acbbb032df2943a4abf85
JorgeAlbertoDiaz <54208302+JorgeAlbertoDiaz@users.noreply.github.com>
2022-12-16 11:12:53 -0300

    Fix: Change for the stable version of lunarvim
    the nightly version has an error with packer...

--- scripts/src/neovim.sh
+++ scripts/src/neovim.sh
@@ -11,7 +11,7 @@ if [ $? -ne 0 ]
 then
 	printf "\n- ${PACKAGE} : ${RED}no instalado${DEFAULT}\n\n"
 	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb  -O neovim.deb &&  sudo dpkg -i neovim.deb && rm -f neovim.deb
-	bash < (curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+	LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 else
 	printf "\n+ ${PACKAGE} : ${GREEN}instalado${DEFAULT}\n\n"
 fi
